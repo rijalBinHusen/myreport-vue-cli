592307d64f0ab0bf4b104d96f0a68e3910df3067
rijal <jeryzal333@gmail.com>
2022-03-11 12:25:48 +0700

    export data in progress

--- src/components/pages/Impor.vue
+++ src/components/pages/Impor.vue
@@ -1,9 +1,41 @@
 <template>
-    <h1>Ini adalah import</h1>
+    <h1 class="w3-center">Export / Import data</h1>
+    <Button primary value="Export" type="button" @trig="expor" />
 </template>
 
 <script>
+import Button from "../elements/Button.vue"
+
 export default {
-    name: "Impor"
+    name: "Impor",
+    components: {
+        Button
+    },
+    methods: {
+        async expor() {
+      // tutup Loader
+    //   this.$store.dispatch("Modal/loading", "close");
+      //append time to export record
+    //   this.$store.dispatch("ExIm/exportAppend");
+    this.$store.dispatch("Expor/expor")
+    // .then((val) => {
+        // console.log(val)
+        // var a = document.createElement("a");
+        // var file = new Blob(
+        //   [JSON.stringify(val)],
+        //   {
+        //     type: "text/plain",
+        //   }
+        // );
+        // a.href = URL.createObjectURL(file);
+        // a.download =
+        //   "All data absensi.js";
+        // a.click();
+
+    // })
+      //destroy data collect
+    //   this.$store.dispatch("ExIm/destroyDataCollect");
+        }
+    }
 }
 </script>
