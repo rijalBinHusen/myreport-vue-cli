923d0ff0b09081c9425d0d7372b4ccea0dda52d6
rijal <jeryzal333@gmail.com>
2022-03-17 19:35:13 +0700

    name fixed with new localbase func

---
+++ src/myfunction.js
@@ -0,0 +1,252 @@
+import Localbase from "localbase";
+
+// initiate new db
+let db = new Localbase("myreport");
+// ---------------------- function to setup the store
+
+function storenya(store, split, period) {
+  //value = {store: "namastore", split: "tahun/bulan/false", period: "202203/time()"}
+
+  //-----------------------------fungsi waktu splitter---------------
+  //202203
+  const a001 = period ? new Date(period) : new Date();
+  const a003 = a001.getMonth();
+  const a004 = a001.getFullYear();
+  const splitter = a004 + (a003 + 1 > 8 ? a004 : "0" + (a003 + 1));
+
+  //-----------------------------------------------------------
+
+  //jika split
+  if (split) {
+    //jika pisah bulan
+    if (split === "bulan") {
+      return store.toLowerCase() + splitter;
+    }
+    // jika pisah tahun
+    else if (split === "tahun") {
+      return store.toLowerCase() + splitter.slice(0, 4);
+    }
+  }
+  return store.toLowerCase();
+}
+
+// ---------------------- function to setup the store
+
+// crud function to indexeddb
+
+export default {
+  append: function (value) {
+    /*value = {
+	store: "nameStore", 
+	split: "tahun/bulan/false",
+	period: "202203/time()"
+	obj: {key: 'value', obj: 'to input to store'}, 
+    } */
+
+    //{store: "namastore", split: "tahun/bulan/false", period: "202203/time()"}
+    db.collection(storenya(value.store, value.split, value.period)).add(
+      value.obj
+    );
+  },
+  update: function (value) {
+    /*value = {
+		store: "nameStore",
+		split: "tahun/bulan/false",
+		period: "202203/time()"
+		obj: {id: 'iddata', key: 'value', obj: 'value to update'}, 
+    } */
+    db.collection(storenya(value.store, value.split, value.period))
+      .doc({ id: value.obj.id })
+      .update(value.obj);
+  },
+  reWrite: function (value) {
+    /*value = {
+	store: "nameStore",
+	id: idData
+	obj: [{key: 'value', obj: 'value to update'}], 
+	split: "tahun/bulan/false",
+	period: "202203/time()"
+    	} */
+    //(nameStore, [{id: idData}, {key: 'new value'}])
+    //write data
+    db.collection(storenya(value.store, value.split, value.period))
+      .doc({ id: value.id })
+      .set(value.obj);
+  },
+  getData: function (deData) {
+    /*deData = {
+	store: "nameStore", 
+	split: "tahun/bulan/false",
+	period: "202203/time()"
+	'orderBy': keyData, 
+	'desc': Boolean, 
+	'limit': number, 
+	withKey: true
+    	} */
+    let store = storenya(deData.store, deData.split, deData.period);
+
+    //limit order desc
+    if (deData.limit && deData.orderBy && deData.desc) {
+      return db
+        .collection(store)
+        .orderBy(deData.orderBy, "desc")
+        .limit(deData.limit)
+        .get();
+    }
+    //limit order
+    if (deData.limit && deData.orderBy) {
+      return db
+        .collection(store)
+        .orderBy(deData.orderBy)
+        .limit(deData.limit)
+        .get();
+    }
+    //limit
+    if (deData.limit) {
+      return db.collection(store).limit(deData.limit).get();
+    }
+    //order desc
+    if (deData.orderBy && deData.desc) {
+      return db.collection(store).orderBy(deData.orderBy, "desc").get();
+    }
+    //order
+    if (deData.orderBy) {
+      return db.collection(store).orderBy(deData.orderBy).get();
+    }
+
+    // if get all data withKey
+    if (deData.withKey) {
+      return db.collection(store).get({ keys: true });
+    }
+    //   all data without key
+    return db.collection(store).get();
+  },
+  findData: function (value) {
+    /*value = {
+	store: "nameStore", 
+	split: "tahun/bulan/false",
+	period: "202203/time()"
+	obj: {key: value}
+    	} */
+    return db
+      .collection(storenya(value.store, value.split, value.period))
+      .doc(value.obj)
+      .get();
+  },
+  deleteCollection: function (value) {
+    /*value = {
+	store: "nameStore", 
+	split: "tahun/bulan/false",
+	period: "202203/time()"
+    	} */
+    db.collection(storenya(value.store, value.split, value.period)).delete();
+  },
+  deleteDb: function () {
+    db.delete();
+  },
+  emptyStore: function (value) {
+    /*value = {
+	store: "nameStore", 
+	split: "tahun/bulan/false",
+	period: "202203/time()"
+    	} */
+    db.collection(storenya(value.store, value.split, value.period)).set({});
+  },
+  deleteDocument: function (value) {
+    /*value = {
+	store: "nameStore", 
+	split: "tahun/bulan/false",
+	period: "202203/time()"
+	id: idData
+    } */
+    //keyword = {key: value}
+    db.collection(storenya(value.store, value.split, value.period))
+      .doc({ id: value.id })
+      .delete();
+  },
+  generateId: function (id, waktu) {
+    //DIV
+    let masterId = id.slice(0, 3);
+    //0003 as 3 on will be +1
+    let increment = Number(id.slice(3)) + 1 + "";
+
+    if (waktu) {
+      //ABS21080001
+      increment = Number(id.slice(-4)) + 1 + "";
+      let fullYear = new Date().getFullYear() + "";
+      let monthNow = new Date().getMonth() + 1;
+      let year = id.slice(3, 5); //21
+      let month = id.slice(5, 7); //08
+      //if the month same
+      if (monthNow === Number(month)) {
+        masterId += year + month;
+      }
+      //if the month not same
+      else {
+        // if the month 9 change to 09
+        monthNow = monthNow < 9 ? "0" + monthNow : monthNow;
+        masterId += fullYear.slice(2) + monthNow;
+        increment = "0";
+      }
+    }
+    //0000
+    let length = "0000".slice(increment.length);
+
+    //DIV0001
+    return masterId + length + increment;
+  },
+  tunggu: function (time) {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(), time);
+    });
+  },
+  ribuan: function (a) {
+    return a.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  },
+  dateFormat: function (a) {
+    // a = ["full", new Date()]
+    const a001 = a[1] ? new Date(a[1]) : new Date();
+    const a002 = a001.getDate();
+    const a003 = a001.getMonth();
+    const a004 = a001.getFullYear();
+    const a005 = a001.getHours() > 9 ? a001.getHours() : "0" + a001.getHours();
+    const a006 =
+      a001.getMinutes() > 9 ? a001.getMinutes() : "0" + a001.getMinutes();
+    const a007 = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "Mei",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Des",
+    ];
+
+    if (a[0] == "time") {
+      return a001.getTime();
+    } //dapatkan waktu dalam bentuk mili second
+    else if (a[0] == "full") {
+      return a002 + " " + a007[a003] + " " + a004 + " " + a005 + ":" + a006;
+    } //dapatkan waktu penuh dd mmm yyyy hh:mm
+    else if (a[0] == "+1") {
+      return a001.getTime() - 25200000 + 86400000;
+    } // hari selanjutnya pada jam 00:00
+    else if (a[0] == "-1") {
+      return a001.getTime() - 25200000 - 86400000;
+    } // hari sebelumnya pada jam 00:00
+    else if (a[0] == "0") {
+      return a001.getTime() - 25200000;
+    } // hari yang tersebut pada jam 00:00
+    else if (a[0] == "dateMonth") {
+      return a002 + "-" + a007[a003];
+    } //dapatkan tanggal bulan dd-mmm
+    else if (a[0] === "yearMonth") {
+      return a004 + (a003 + 1 > 9 ? a003 : "0" + (a003 + 1));
+    } //dapatkan tahun bulan, yyyym
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from "vuex";
-import Localbase from "../Localbase";
+import myfunction from "../myfunction";
 import Navbar from "./modules/Navbar";
 import Modal from "./modules/Modal";
 import Collect from "./modules/Collect";
@@ -9,112 +9,114 @@ import Expor from "./modules/Expor";
 
 export default createStore({
   state: {
-    store: ["Name", "Uncollected"],
+    store: ["Name"],
   },
   mutations: {},
   actions: {
-    append({ commit }, value) {
-      /* 
-       value = {
-         store: 'nameOfStore', 
-         obj: {object: 'to append to indexeddb'}, 
-         id: String,
-         waktu: Boolean,
-         split: "getTime() / 2022-03-22 / or antoher date format"
-        } 
+    append({ commit, rootGetters }, value) {
+      /* value = { 
+            store: "nameOfStore",
+            obj: { key: 'value', obj: 'to input to store' },
+            id: "id" //optional
+          } 
        the first letter of value.store must be capital e.g 'Group'
        */
 
+      let objToSend = Object.assign(rootGetters[`${value.store}/store`], {
+        obj: value.obj,
+      });
+
       // create id to the record
       if (value.id) {
-        if (value.waktu) {
-          value.obj.id = Localbase.generateId(value.id, true);
-        } else value.obj.id = Localbase.generateId(value.id);
+        if (objToSend.split) {
+          objToSend.obj.id = myfunction.generateId(value.id, true);
+        } else objToSend.obj.id = myfunction.generateId(value.id);
       }
       // commit to module e.g 'Group/append
       commit(`${value.store}/append`, value.obj, { root: true });
 
       // insert record to indexeddb and return as promise
       return new Promise((resolve) => {
-        let dbstore;
-        // jika split maka store akan ditambah dengan 20223
-        if (value.split) {
-          dbstore =
-            value.store + Localbase.dateFormat(["yearMonth", value.split]);
-        }
-        // jika tidak split
-        else {
-          dbstore = value.store;
-        }
-        Localbase.append(dbstore.toLowerCase(), value.obj);
+        myfunction.append(objToSend);
         setTimeout(() => resolve(), 330);
       });
     },
 
-    delete({ commit }, value) {
-      // value = { store: "listsnames", doc: {id: 001 }, split: "202203"}
-      let dbstore;
-      // jika split maka store akan ditambah dengan 20223
-      if (value.doc.id.length === 11 && value.split) {
-        dbstore =
-          value.store + Localbase.dateFormat(["yearMonth", value.split]);
-      }
-      // jika tidak split
-      else {
-        dbstore = value.store;
-      }
+    delete({ commit, rootGetters }, value) {
+      // value = { store: "listsnames", id: 001 }
+
+      let objToSend = Object.assign(rootGetters[`${value.store}/store`], {
+        id: value.id,
+      });
       //delete from state
       commit(`${value.store}/delete`, value.doc.id, { root: true });
-
+      // if the store is split
+      if (objToSend.split) {
+        objToSend.period =
+          value.id.slice(3, 7) + "-" + value.id.slice(7, 9) + "-01";
+      }
       // delete record from indexeddb and return as promise
       return new Promise((resolve) => {
-        Localbase.deleteDocument(dbstore.toLowerCase(), value.doc);
+        myfunction.deleteDocument(objToSend);
         setTimeout(() => resolve(), 330);
       });
     },
 
     // to update record in indexeddb
-    update({ commit }, value) {
-      // value = {store: 'nameOfStore', obj: {id: idOfDocument, obj: {asd: 'to append to indexeddb'}, split: "202203" }
+    update({ commit, rootGetters }, value) {
+      // value = {store: 'nameOfStore', obj: {id: idOfDocument, obj: {asd: 'to append to indexeddb'} }
       // the first letter of value.store must be capital e.g 'Group'
-      // console.log(value.split);
-      // console.log(value.obj.id.length);
-      let dbstore;
-      // jika split maka store akan ditambah dengan 20223
-      if (value.obj.id.length === 11 && value.split) {
-        dbstore =
-          value.store + Localbase.dateFormat(["yearMonth", value.split]);
-      }
-      // jika tidak split
-      else {
-        dbstore = value.store;
+
+      /*value = {
+        store: "nameStore",
+        split: "tahun/bulan/false",
+        period: "202203/time()"
+        obj: {id: 'iddata', key: 'value', obj: 'value to update'}, 
+    } */
+
+      let objToSend = Object.assign(rootGetters[`${value.store}/store`], {
+        obj: value.obj,
+      });
+
+      if (objToSend.split) {
+        objToSend.period =
+          value.obj.id.slice(3, 7) + "-" + value.id.slice(7, 9) + "-01";
       }
 
       // send to indexeddb
-      Localbase.update(
-        value.store.toLowerCase(),
-        { id: value.obj.id },
-        value.obj
-      );
+      myfunction.update(objToSend);
       // send to module
       commit(`${value.store}/update`, value.obj, { root: true });
     },
 
-    getStart({ commit, state }) {
+    getStart({ commit, state, rootGetters }) {
+      /*deData = {
+        store: "nameStore", 
+        split: "tahun/bulan/false",
+        period: "202203/time()"
+        'orderBy': keyData, 
+        'desc': Boolean, 
+        'limit': number,
+    	} */
       // iterate the store
       state.store.forEach((val) => {
         // call the get data functions
-        Localbase.getData({
-          store: val.toLowerCase(),
-          orderBy: "id",
-          desc: true,
-        }).then((result) =>
-          commit(`${val}/${val.toLowerCase()}`, result, { root: true })
-        );
+        myfunction
+          .getData(
+            Object.assign(rootGetters[`${val}/store`], {
+              orderBy: "id",
+              desc: true,
+              limit: 100,
+            })
+          )
+          .then((result) =>
+            commit(`${val}/${val.toLowerCase()}`, result, { root: true })
+          );
+        console.log();
       });
     },
     getAllData({ commit }, value) {
-      return Localbase.getData({ store: value.toLowerCase(), withKey: true });
+      return myfunction.getData({ store: value.toLowerCase(), withKey: true });
     },
   },
   getters: {
