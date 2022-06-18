2c82ce9867a091e5cccdd7f504617e8dec16cb8f
rijal <jeryzal333@gmail.com>
2022-06-19 05:35:29 +0700

    Backup data base on summary store

--- src/store/modules/Expor.js
+++ src/store/modules/Expor.js
@@ -16,19 +16,21 @@ const Expor = {
     async expor({ state, commit, rootState, dispatch, rootGetters }) {
       commit("Modal/active", { judul: "", form: "Loader" }, { root: true });
       commit("replace", {});
-      // get all store from state
-      let storeName = JSON.parse(JSON.stringify(rootState.store));
+      // get the summary information
+      let storeName = await dispatch("getAllData", "Summary", { root: true })
+      // console.log(storeName)
+      commit("append", { store: "summary", obj: storeName });
       // iterate, push to variable and waiting
       for (let i = 0; i < storeName.length; i++) {
         // wait until data commit to state
-        let result = await dispatch("getAllData", storeName[i], { root: true })
+        let result = await dispatch("getAllData", storeName[i]?.key, { root: true })
         // if result > 0
         if(result?.length) {
           // append to state this state
-          commit("append", { store: storeName[i], obj: result });
+          commit("append", { store: storeName[i]?.key, obj: result });
         }
       }
-      // create a download file
+    //   // create a download file
       var a = document.createElement("a");
       // new blob data
       var file = new Blob([JSON.stringify(state.lists)], { type: "text/plain" });
