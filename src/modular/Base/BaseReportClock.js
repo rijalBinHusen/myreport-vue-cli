8550fd8c40e6e15544f2c75e6662500ded8d89db
rijal <jeryzal333@gmail.com>
2022-07-11 09:21:30 +0700

    fix bug basereport clock

--- src/modular/Base/BaseReportClock.js
+++ src/modular/Base/BaseReportClock.js
@@ -130,34 +130,38 @@ const BaseReportClock = {
         let totalDo = 0;
         let totalKendaraan = 0;
         let totalWaktu = 0;
-        let filtered = JSON.parse(JSON.stringify(state.lists)).filter(
-          (val) => val.shift == shift && val.parent == id
-        );
-        filtered.forEach((val) => {
-          totalDo += 1;
-          totalKendaraan += 1;
-          // start
-          let start = rootGetters["dateFormat"]({
-            format: "time",
-            time: `2022-03-03 ${val.start.slice(0, 2)}:${val.start.slice(
-              3,
-              5
-            )}`,
-          });
-          //finish
-          let finish = rootGetters["dateFormat"]({
-            format: "time",
-            time: `2022-03-03 ${val.finish.slice(0, 2)}:${val.finish.slice(
-              3,
-              5
-            )}`,
-          });
-          // jika finish lebih kecil dari pada start, maka finish ditambah 24 jam guys (86400000)
-          // finish - start
-          let total =
-            finish < start ? finish + 86400000 - start : finish - start;
-          // jaddikan menit, masukan total waktu
-          totalWaktu += total / 1000 / 60 - val.rehat * 60;
+        [...state.lists].forEach((val) => {
+          if (
+            val.shift == shift &&
+            val.parent == id &&
+            val.start.length == 5 &&
+            val.finish.length == 5
+          ) {
+            totalDo += 1;
+            totalKendaraan += 1;
+            // start
+            let start = rootGetters["dateFormat"]({
+              format: "time",
+              time: `2022-03-03 ${val.start.slice(0, 2)}:${val.start.slice(
+                3,
+                5
+              )}`,
+            });
+            //finish
+            let finish = rootGetters["dateFormat"]({
+              format: "time",
+              time: `2022-03-03 ${val.finish.slice(0, 2)}:${val.finish.slice(
+                3,
+                5
+              )}`,
+            });
+            // jika finish lebih kecil dari pada start, maka finish ditambah 24 jam guys (86400000)
+            // finish - start
+            let total =
+              finish < start ? finish + 86400000 - start : finish - start;
+            // jaddikan menit, masukan total waktu
+            totalWaktu += total / 1000 / 60 - val.rehat * 60;
+          }
         });
         return {
           totalDo: totalDo,
