993dee7266007742a358b67d749201a6dec10400
rijal <jeryzal333@gmail.com>
2022-07-25 20:24:00 +0700

    adding dist_electron to gitignore

--- src/composable/storeSyncData.js
+++ src/composable/storeSyncData.js
@@ -1,11 +1,13 @@
+// dapatkan store activity
+
 // jika store activity di indexeddb tidak ada
-    // maka ambil store summary,
-    // iterate untuk ambil nama store
-    // setelah dapat nama store, ambil semua data di indexeddb atas nama store tersebut
-    // kirim ke firebase
+// maka ambil store summary,
+// iterate untuk ambil nama store
+// setelah dapat nama store, ambil semua data di indexeddb atas nama store tersebut
+// kirim ke firebase
 
 // jika ditemukan store activity
-    // periksa activity terakhir
-    // bandingkan dengan activity store yang ada difirebase
-    // jika lebih baru yang difirebase, ambil data dari firebase, simpan ke indexeddb
-    // jika lebih baru yang di indexeddb, ambil data dari indexeddb, kirim ke firebase
+// periksa activity terakhir
+// bandingkan dengan activity store yang ada difirebase
+// jika lebih baru yang difirebase, ambil data dari firebase, simpan ke indexeddb
+// jika lebih baru yang di indexeddb, ambil data dari indexeddb, kirim ke firebase
