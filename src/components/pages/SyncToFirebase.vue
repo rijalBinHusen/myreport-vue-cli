1f01dd2a053e1d7ed987ffadab2f6e8cac830e09
rijalBinHusen <jeryzal333@gmail.com>
2022-07-25 16:19:51 +0700

    Continue later

---
+++ src/components/pages/SyncToFirebase.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="handleButton">
+    Sync
+  </button>
+</template>
+
+<script>
+export default {
+    setup() {
+        const handleButton = () => {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/composable/storeSyncData.js
@@ -0,0 +1,11 @@
+// jika store activity di indexeddb tidak ada
+    // maka ambil store summary,
+    // iterate untuk ambil nama store
+    // setelah dapat nama store, ambil semua data di indexeddb atas nama store tersebut
+    // kirim ke firebase
+
+// jika ditemukan store activity
+    // periksa activity terakhir
+    // bandingkan dengan activity store yang ada difirebase
+    // jika lebih baru yang difirebase, ambil data dari firebase, simpan ke indexeddb
+    // jika lebih baru yang di indexeddb, ambil data dari indexeddb, kirim ke firebase
