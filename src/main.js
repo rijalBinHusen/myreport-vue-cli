5cf5caa3d583835e095bd7d39a5d0793700f8e1b
rijal <jeryzal333@gmail.com>
2022-07-26 20:37:44 +0700

    user keep logged in when page refresh

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from './router';
+import router from "./router";
 import "w3-css";
 import store from "./store";
+import { auth } from "./firebase/firebaseApp";
 
-createApp(App).use(store).use(router).mount("#app");
+let app;
+
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App).use(store).use(router).mount("#app");
+  }
+});
