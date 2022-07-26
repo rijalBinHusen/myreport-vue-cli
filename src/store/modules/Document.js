f47e56f5313a50be8265c68e824bae175cff5a2e
rijalBinHusen <jeryzal333@gmail.com>
2022-07-26 07:36:19 +0700

    fix collected component bug

--- src/store/modules/Document.js
+++ src/store/modules/Document.js
@@ -352,6 +352,7 @@ const Uncollected = {
                   val?.warehouse
                 )?.name,
                 itemVariance: val.itemVariance,
+                isfinished: val.isfinished,
               });
             }
           });
