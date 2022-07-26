b8618f4359180b61df8cf8999e7f7e3c6e7ccf02
rijalBinHusen <jeryzal333@gmail.com>
2022-07-26 16:05:49 +0700

    success get all store in idexeddb

--- src/composable/storeSyncData.js
+++ src/composable/storeSyncData.js
@@ -1,22 +1,34 @@
-import getCollection from "./storeGetAllDocuments";
+import func from "../myfunction"
 
-const syncData = () => {
+const syncData = async () => {
   // dapatkan store activity
-  const { error, documents } = getCollection("activity");
-
-  console.log(documents);
+  let activity = await func.getData({store: "activity"})
+  let allStores = []
+  
+  // jika store activity di indexeddb tidak ada
+  if(!activity.length) {
+    
+    // maka ambil store summary,
+    let summary = await func.getData({store: "summary", withKey: true})
+    // iterate untuk ambil nama store
+    summary.forEach((val) => {
+      // setelah dapat nama store, ambil semua data di indexeddb atas nama store tersebut
+      allStores.push(func.getData({store: val?.key}).then((res) => {
+        return {
+          store: val?.key,
+          data: res
+        }
+      }))
+    })
+  }
+  console.log(await Promise.all(allStores))
+  // kirim ke firebase
+  
+  // jika ditemukan store activity
+  // periksa activity terakhir
+  // bandingkan dengan activity store yang ada difirebase
+  // jika lebih baru yang difirebase, ambil data dari firebase, simpan ke indexeddb
+  // jika lebih baru yang di indexeddb, ambil data dari indexeddb, kirim ke firebase
+  
 };
-
-// jika store activity di indexeddb tidak ada
-// maka ambil store summary,
-// iterate untuk ambil nama store
-// setelah dapat nama store, ambil semua data di indexeddb atas nama store tersebut
-// kirim ke firebase
-
-// jika ditemukan store activity
-// periksa activity terakhir
-// bandingkan dengan activity store yang ada difirebase
-// jika lebih baru yang difirebase, ambil data dari firebase, simpan ke indexeddb
-// jika lebih baru yang di indexeddb, ambil data dari indexeddb, kirim ke firebase
-
 export default syncData;
