90027e6e68ce18a49632583be7bc4671b03f841b
rijalBinHusen <jeryzal333@gmail.com>
2022-07-29 09:38:40 +0700

    continue later

--- src/composable/storeSyncData.js
+++ src/composable/storeSyncData.js
@@ -1,31 +1,25 @@
 import func from "../myfunction";
-import addDocument from "./storeAdd";
+import addDocument from "./firebaseAddStore";
 
-const syncData = async () => {
-  // dapatkan store login
-  let login = await func.getData({ store: "login" });
-  // console.log(login)
-  // bandingkan login terakhir dengan yang ada difirebase
-
+const syncData = () => {
   // jika ditemukan store activity
-  // let activity = await func.findData({ store: "activity",  });
-  // periksa activity terakhir
-  // bandingkan dengan activity store yang ada difirebase
-  // console.log(activity)
+  let activity = func.getData({ store: "activity"});
 
     // console.log(await Promise.all(allStores));
-  //   return Promise.all(allStores).then(async (val) => {
-  //     //   iterate name of store
-  //     for (let rec of val) {
-  //       // iterate data that contain in name of store
-  //       for (let doc of rec?.data) {
-  //         // kirim ke firebase
-  //         // await addDocument(rec?.store, doc?.key, doc?.data);
-  //         console.log(rec?.store, doc?.key, doc?.data)
-  //       }
-  //     }
-  // });
-  // jika lebih baru yang difirebase, ambil data dari firebase, simpan ke indexeddb
-  // jika lebih baru yang di indexeddb, ambil data dari indexeddb, kirim ke firebase
+    return new Promise( resolve => {
+      activity.then(async (val) => {
+        //   iterate name of store
+        for (let rec of val) {
+          // iterate data that contain in name of store
+          let record = await func.findData({ store: rec?.store, criteria: { id: rec?.idRecord }})
+          console.log(record, rec?.store, rec?.idRcord)
+          //   // kirim ke firebase
+          //   // await addDocument(rec?.store, doc?.key, doc?.data);
+          //   console.log(rec?.store, doc?.key, doc?.data)
+          await func.tunggu(3000)
+        }
+        resolve()
+      })
+    });
 };
 export default syncData;
