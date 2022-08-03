b5a0ff026dc52a5a755e6d98b086f92bc0dbdc40
rijalBinHusen <jeryzal333@gmail.com>
2022-08-03 08:59:14 +0700

    finish for now

--- src/composable/storeSyncData.js
+++ src/composable/storeSyncData.js
@@ -8,15 +8,34 @@ const syncData = () => {
     // console.log(await Promise.all(allStores));
     return new Promise( resolve => {
       activity.then(async (val) => {
+        let inserted = {}
         //   iterate name of store
+        // iterate data that contain in name of store
         for (let rec of val) {
-          // iterate data that contain in name of store
-          let record = await func.findData({ store: rec?.store, criteria: { id: rec?.idRecord }})
-          console.log(record, rec?.store, rec?.idRcord)
-          //   // kirim ke firebase
-          //   // await addDocument(rec?.store, doc?.key, doc?.data);
-          //   console.log(rec?.store, doc?.key, doc?.data)
-          await func.tunggu(3000)
+          if(rec?.store != 'activity' && rec?.store != 'summary' && rec?.store != 'login') {
+            // periksa apakah store sudah pernah diinsert?
+            if(inserted[rec?.store]) {
+              //  jika sudah pernah diinsert, periksa apakah record sudaah pernah diinsert ke firebase]
+              if(inserted[rec?.store].includes(rec?.idRecord)) {
+
+              }
+              else {
+                // jika tidak pernah diinsert
+                inserted[rec?.store].push(rec?.idRecord)
+              }
+
+            } 
+            else {
+              // jika store tidaak pernah diinsert
+              inserted[rec?.store] = [rec?.idRecord]
+            }
+            
+            let record = await func.findData({ store: rec?.store, criteria: { id: rec?.idRecord }})
+            //   // kirim ke firebasel
+            //   // await addDocument(rec?.store, doc?.key, doc?.data);
+            //   console.log(rec?.store, doc?.key, doc?.data)
+            await func.tunggu(3000)
+          }
         }
         resolve()
       })
