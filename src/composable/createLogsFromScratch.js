4973c4ea826db71889e9ef59d86b020781b2eae7
rijalBinHusen <jeryzal333@gmail.com>
2022-08-08 10:38:29 +0700

    dont push activity to firebase

--- src/composable/createLogsFromScratch.js
+++ src/composable/createLogsFromScratch.js
@@ -1,9 +1,6 @@
 import func from "../myfunction";
-import addDocument from "./firebaseAddStore";
 
 const createLogsFromScratch = async () => {
-  //dapatkan store activity
-  let activity = await func.getData({ store: "activity" });
   let allStores = [];
 
   // jika store activity di indexeddb tidak ada
@@ -34,7 +31,7 @@ const createLogsFromScratch = async () => {
             // iterate data that contain in name of store
             for (let i = 0; i <= rec?.data.length; i++) {
               let doc = rec?.data[i]
-              if(doc?.data?.id && rec?.store !== 'activity' && i < 500) {
+              if(doc?.data?.id && rec?.store !== 'activity') {
                 // jika store tidak pernah diinsert
                 if(!inserted[rec?.store]) {
                   inserted[rec?.store] = []
@@ -42,11 +39,7 @@ const createLogsFromScratch = async () => {
                 // jika record belum pernah diinsert
                 if(!inserted[rec?.store].includes(doc?.key)) {
                   inserted[rec?.store].push(doc?.key)
-                  let res = await func.addActivity({ type: "create", store: rec?.store, idRecord: doc?.data?.id })
-                  // insert to firebase
-                  await addDocument(rec?.store, doc?.key, doc?.data);
-                  await addDocument('activity', res?.id, res);
-
+                  await func.addActivity({ type: "create", store: rec?.store, idRecord: doc?.data?.id })
                 }
               }
             }
