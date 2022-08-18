7e7d745093941dc8f3a54bb2a4dbeed452da9c28
rijalBinHusen <jeryzal333@gmail.com>
2022-08-18 14:57:40 +0700

    continue later

--- src/composable/storeSyncData.js
+++ src/composable/storeSyncData.js
@@ -1,35 +1,44 @@
 import { db } from "@/firebase/firebaseApp";
-import { collection, getDocs } from "firebase/firestore"
+import { doc, getDoc } from "firebase/firestore"
 import func from "../myfunction"
 import addData from "./firebaseAddStore"
 
 // the store that we're gonna sync
-let store = ['problem']
+let stores = ['problem']
 
 import getStore from "./firebaseGetAllDocument";
-// cek dulu apakah pernah di sync sebelumnya
 const startSyncingData = async () => {
-  const querySnapshot = await getDocs(collection(db, 'sync'));
-  // 	kalau ada, periksa tanggal berapa dia sync
-  // 	ambil activitas store terakhir
-  // 	periksa tanggal sync firebase dengan aktivitas terakhir
-  // 	jika lebih besar aktivitas terakhir sync ke firebase
-// 	catat tanggal sync difirebase
-
-// jika tidak pernah ada sync sebelumnya
-  if(querySnapshot?.empty) {
-    store.forEach(async (store) => {
-      // 	get semua data yang ada distore
-      await func.findData({ store: store, criteria: { isFinished: false }}).then(async (docs) => {
-        // 	push semua data ke firebase
-        for (let doc of docs) {
-          await addData(store, doc?.id, doc)
-        }
-
-      addData('synced', store, { login: localStorage.getItem('loginya'), time: new Date().getTime() })
-      })
-    })
+  // cek dulu apakah pernah di sync sebelumnya
+  for(let store of stores) {
+    const isSynced = await getDoc(doc(db, 'synced', store));
+    // 	kalau ada, periksa tanggal berapa dia sync
+    if(isSynced.exists()) {
+      console.log(isSynced.data())
+      // func.findData({ store: 'activity', criteria: { isFinished: false }})
+      // ambil 30 login terakhir
+      // bandingkan login yang ada di store sync didatabase
+      // ambil activitas store terakhir
+      // periksa tanggal sync firebase dengan aktivitas terakhir
+      // jika lebih besar aktivitas terakhir sync ke firebase
+      // catat tanggal sync difirebase
+    }
   }
+//   const querySnapshot = await getDocs(collection(db, 'synced'));
+
+// // jika tidak pernah ada sync sebelumnya
+//   if(querySnapshot?.empty) {
+//     store.forEach(async (store) => {
+//       // 	get semua data yang ada distore
+//       await func.findData({ store: store, criteria: { isFinished: false }}).then(async (docs) => {
+//         // 	push semua data ke firebase
+//         for (let doc of docs) {
+//           await addData(store, doc?.id, doc)
+//         }
+
+//       addData('synced', store, { login: localStorage.getItem('loginya'), time: new Date().getTime() })
+//       })
+//     })
+//   }
 
 // ========
 // berarti kita butuh document tersendiri berjudul sync
