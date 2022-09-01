356ad45d7f48077b15827c22c7d3f62a5ce84e6b
rijalBinHusen <jeryzal333@gmail.com>
2022-09-01 14:39:18 +0700

    subscribe mutation in composable function

---
+++ src/composable/piece/subscribeMutation.js
@@ -0,0 +1,25 @@
+import store from '@/store'
+
+export const subscribeMutation = async (judul, form, obj, subscribeMtation) => {
+    console.log(store)
+    let unsubscribe;
+    // create a promise to waiting the update process, and listen to the tunnel message
+    const prom = new Promise(resolve => {
+            // luncurkan dialog
+            store.commit('Modal/active', { judul: judul,  form: form,  obj: obj })
+            // subscribe untuk tanggkap confirm dialog apakah yes atau tidak
+            unsubscribe = store.subscribe(mutation => {
+                // if the confirmation button clicked whatever yes or no
+                if(mutation?.type == subscribeMtation) {
+                    // resolve the messaage, true or false
+                    resolve(mutation?.payload)
+                }
+            })
+        })
+        // jika oke kirim pesan
+    return prom.then((res) => {
+        unsubscribe()
+        store.commit('Modal/active')
+        return res
+    })
+}
