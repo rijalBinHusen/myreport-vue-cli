4d07cfaa2df144f5cdd5363be49c3452a0fe0711
rijalBinHusen <jeryzal333@gmail.com>
2022-09-08 09:35:14 +0700

    render list field problem

--- src/pages/FieldProblem.vue
+++ src/pages/FieldProblem.vue
@@ -12,41 +12,52 @@
             />
         </div>
     
-        <!-- <DatatableVue
-            :datanya="table?.lists"
-            :heads="table?.heads"
-            :keys="table?.keys"
-            option
+        <DatatableVue
+            v-if="renderTable"
+            :datanya="lists"
+            :heads="['periode', 'supervisor', 'head', 'masalah']"
+            :keys="['periode', 'supervisor', 'head', 'masalah']"
             id="tableFieldProblem"
         >
         
-            <template #default="{ prop }">
+            <!-- <template #default="{ prop }">
                 <Button v-if="!prop?.inserted && !inserted" value="Delete" :datanya="prop.id" danger type="button" class="w3-tiny" @trig="remove($event)"/>
                 <Button v-if="!inserted" value="Insert" primary type="button" class="w3-tiny" @trig="insertCase(prop)"/>
                 <Button v-else value="Edit" secondary type="button" class="w3-tiny" @trig="edit(prop)"/>
                 
             </template> -->
-        <!-- </DatatableVue>  -->
+        </DatatableVue> 
         
     </div>
 </template>
     
-    <script>
-    import ButtonVue from '@/components/elements/Button.vue'
-    import DatatableVue from '@/components/parts/Datatable.vue'
-    import { fieldProblem } from '@/composable/piece/vuexModalLauncher'
-    
-    export default {
-        setup() {
-            
-            return { fieldProblem }
-        },
-        data() {
-            return {}
-        },
-        components: {
-            ButtonVue,
-            DatatableVue
-        },
-    }
-    </script>
+<script>
+import ButtonVue from '@/components/elements/Button.vue'
+import DatatableVue from '@/components/parts/Datatable.vue'
+import { fieldProblem } from '@/composable/piece/vuexModalLauncher'
+import { listsFieldProblem } from '@/composable/components/FieldProblem'
+import { reactive, ref } from '@vue/reactivity'
+import { onMounted } from '@vue/runtime-core'
+
+export default {
+    setup() {
+        const lists = ref([])
+        const renderTable = ref(false)
+        const table = {
+            heads : ['periode', 'supervisor', 'head', 'masalah'],
+            keys: ['periode', 'supervisor', 'head', 'masalah']
+        }
+
+        onMounted(async () => {
+            lists.value = await listsFieldProblem()
+            renderTable.value = true
+        })
+
+        return { fieldProblem, table, lists, renderTable }
+    },
+    components: {
+        ButtonVue,
+        DatatableVue
+    },
+}
+</script>
