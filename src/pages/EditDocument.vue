de58443568df90b8de0ca845e8efdb755b935ae6
rijalBinHusen <jeryzal333@gmail.com>
2023-07-20 15:50:45 +0700

    show documents in edit document page

--- src/pages/EditDocument.vue
+++ src/pages/EditDocument.vue
@@ -38,63 +38,37 @@
 </div>
 </template>
 
-<script>
-import Button from "@/components/elements/Button.vue"
-import Datatable from "@/components/parts/Datatable.vue"
-import { lists as listsOfDocuments, Documents } from './Documents/DocumentsPeriod'
-import { subscribeMutation } from '@/composable/piece/subscribeMutation'
-import { ref } from '@vue/reactivity'
-import { useStore } from 'vuex'
-import { onMounted } from '@vue/runtime-core'
+<script setup>
+    import Button from "@/components/elements/Button.vue"
+    import Datatable from "@/components/parts/Datatable.vue"
+    import { lists, Documents } from './Documents/DocumentsPeriod'
+    import { subscribeMutation } from '@/composable/piece/subscribeMutation'
+    import { useStore } from 'vuex'
 
-export default {
-    setup() {
-        const lists = ref([])
-        const store = useStore()
-        const { getDocuments, removeDocument } = Documents()
+    const store = useStore()
+    const { getDocuments, removeDocument } = Documents();
 
-        const pickPeriode = async () => {
-            let period = await subscribeMutation("Set periode to show", "PeriodePicker",  false, 'Modal/tunnelMessage')
-            if(period) {
-                //open the loader
-                store.commit("Modal/active", {judul: "", form: "Loader"})
-                // wait the process
-                await getDocuments(period?.periode1, period?.periode2)
-                //close the loader
-                store.commit("Modal/active")
-                renewLists()
-            }
+    const pickPeriode = async () => {
+        let period = await subscribeMutation("Set periode to show", "PeriodePicker",  false, 'Modal/tunnelMessage')
+        if(period) {
+            //open the loader
+            store.commit("Modal/active", {judul: "", form: "Loader"})
+            // wait the process
+            await getDocuments(period?.periode1, period?.periode2)
+            //close the loader
+            store.commit("Modal/active")
         }
+    }
 
-        const handleButton = async (ev) => {
-            let judul = ev ? 'Edit dokumen' : 'Tambahkan dokumen'
-            let res = await subscribeMutation(judul, 'DocumentSingleForm', { idDocument: ev }, 'Modal/tunnelMessage')
-            if(res) {
-                renewLists()
-            }
+    const handleButton = async (ev) => {
+        let judul = ev ? 'Edit dokumen' : 'Tambahkan dokumen'
+        subscribeMutation(judul, 'DocumentSingleForm', { idDocument: ev }, 'Modal/tunnelMessage')
+    }
+
+    const remove = async (idDocument) => {
+        let res = await subscribeMutation('', 'Confirm', {}, 'Modal/tunnelMessage')
+        if(res) {
+            await removeDocument(idDocument)
         }
-
-        onMounted(() => {
-            renewLists()
-        })
-
-        const renewLists = async () => {
-            lists.value = await listsOfDocuments()
-        }
-
-        const remove = async (idDocument) => {
-            let res = await subscribeMutation('', 'Confirm', {}, 'Modal/tunnelMessage')
-            if(res) {
-                await removeDocument(idDocument)
-                renewLists()
-            }
-        }
-
-        return { lists, pickPeriode, handleButton, remove }
-    },
-    components: {
-        Button,
-        Datatable,
-    },
-}
-</script>@/pages/Documents/DocumentsPeriod
+    }
+</script>
